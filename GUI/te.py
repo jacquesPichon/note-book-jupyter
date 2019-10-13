0a3c0c879380159dab2f53a3017587e91a4cde23
jacques pichon <jacques.pichon@ac-rennes.fr>
2019-10-13 22:00:07 +0200

    mise a jour du sujet  sur le GUI et ajout de l'illustration

---
+++ GUI/te.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+#fonction 
+
+
+fencompteur = Tk() 
+fencompteur.title("Compteur de fréquentation d'un évènement") 
+fencompteur.geometry('600x200')
+fencompteur.configure(bg = 'grey')
+#Les boutons
+
+
+#Les labels pour l'affichage
+
+
+
+fenetre.mainloop()
